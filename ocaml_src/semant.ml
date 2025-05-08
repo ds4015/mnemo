12f8e58a8c8330e183ea0428e66699e4c93919df
Soyoon <sp4412@columbia.edu>
2025-05-08 12:04:15 -0400

    updated semant.ml in src folder

--- ocaml_src/semant.ml
+++ ocaml_src/semant.ml
@@ -1,149 +1,62 @@
-let rec eval expr =
-  match expr with
-  | IfExpr (cond, then_block, elifs, else_block) ->
-    (match eval cond with
-     | Bool true -> eval (Seq then_block)
-     | Bool false ->
-        let rec eval_elif = function
-          | [] -> (match else_block with
-                  | Some block -> eval (Seq block)
-                  | None -> ValUnit)
-          | (cond_expr, block) :: rest ->
-            (match eval cond_expr with
-             | Bool true -> eval (Seq block)
-             | Bool false -> eval_elif rest
-             | _ -> failwith "Elif condition must be a boolean")
-        in
-        eval_elif elifs
-     | _ -> failwith "If condition must be a boolean")
-  | IntLit(i) -> Int(i)
-  | BoolLit(b) -> Bool(b)
-  | LabelLit(l) -> Label(l)
-  | FloatLit(f) -> Float(f)
-  | TextLit(t) -> Text(t)
-  | CodeBlock (_, exprs, _) -> 
-    (match List.rev_map eval exprs with
-     | [] -> failwith "Empty block"
-     | last :: _ -> last)
-  | NodeBlock (_, node, _) -> 
-    (match List.rev_map eval node with
-     | [] -> failwith "Empty block"
-     | last :: _ -> last)
-  | NodeStream (_, nodes, _) -> 
-    (match List.rev_map eval nodes with
-     | [] -> failwith "Empty block"
-     | last :: _ -> last)
-  | Nar (var_name, title, root, narr_label) ->
-    let narr = { title; root; narr_label } in
-    NarrativeTable.add narrative_variables var_name narr; 
-    Narrative(narr)
-  | Itm (var_name, iname, usage, num, unique, dur, cost, cons) -> 
-    let it = { iname; usage; num; unique; dur; cost; cons } in
-    ItemTable.add item_variables var_name it; 
-    Item(it)
-  | Chrc (var_name, name, level, hp, inventory) -> 
-    let chr = { name; level; hp; inventory } in
-    CharacterTable.add character_variables var_name chr;
-    Character(chr) 
-  | Nde (character, id, dialogue, label, options, next) ->
-    add_item_to_queue action_queue "" "";
-    let true_name = ref character in
-    CharacterTable.iter (fun var char ->
-        if var = character then 
-          true_name := char.name else ()
-      ) character_variables;
-    let nd = { character = !true_name; id; dialogue; label; options; next } in
-    NodeTable.add nodes id nd;
-    Node(nd)
-  | Var(v) -> 
-    (try 
-       let i = ItemTable.find item_variables v in
-       Item i
-     with Not_found -> 
-     try 
-       let c = CharacterTable.find character_variables v in
-       Character c
-     with Not_found -> 
-     try 
-       let n = NarrativeTable.find narrative_variables v in
-       Narrative n
-     with Not_found -> 
-       failwith ("Variable not found in any table: " ^ v))
-  | Asn (e1, e2) -> let v2 = eval e2 in v2
-  | Seq(exprs) ->
-    let rec eval_seq = function
-      | [] -> failwith "Empty sequence has no value"
-      | [e] -> eval e
-      | e :: rest -> 
-        let _ = eval e in
-        eval_seq rest
+
+/* menemo smenatic checking */
+
+open Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+
+let check_assignment leftVType rightVType =
+	if leftVType = rightVType then
+		leftVType
+	else
+		raise (Failure ("Type error: left and right types do not match"))
+
+
+let symbols = List.fold_left (fun m (ty, name) -> StringMap.add name ty m)
+    StringMap.add()
+    (* append symbols here *)
+
+
+let ident_type s =
+      try StringMap.find s symbols
+      with Not_found -> raise (Failure ("Unknown identifier " ^ s))
     in
-    eval_seq exprs
-  | Tup (e1, e2) -> let v1 = eval e1 and v2 = eval e2 in 
-    (match v1, v2 with
-     | Text s1, Label s2 -> Tuple(s1, s2)
-     | _ -> failwith "Invalid types for Tup")
-  | Binop (e1, op, e2) ->
-    let v1 = eval e1 and v2 = eval e2 in
-    (match op with
-     | Add -> (match v1, v2 with
-         | Int i1, Int i2 -> Int(i1 + i2)
-         | _ -> failwith "Invalid types for addition")
-     | Sub -> (match v1, v2 with
-         | Int i1, Int i2 -> Int(i1 - i2)
-         | _ -> failwith "Invalid types for subtraction")
-     | Mul -> (match v1, v2 with
-         | Int i1, Int i2 -> Int(i1 * i2)
-         | _ -> failwith "Invalid types for multiplication")
-     | Div ->  if v2 == Int(0) then failwith "Cannot divide by zero" else
-         (match v1, v2 with
-          | Int i1, Int i2 -> Int(i1 / i2)
-          | _ -> failwith "Invalid types for division")
-     | Eq -> (match v1, v2 with
-         | Int i1, Int i2 -> Bool(i1 == i2)
-         | Float f1, Float f2 -> Bool(f1 == f2)
-         | Int i1,  Float f2 -> Bool(float_of_int i1 == f2)
-         | Float f1, Int i2 -> Bool(f1 == float_of_int i2)
-         | _ -> failwith "Invalid types for comparison")
-     | Gt -> (match v1, v2 with
-         | Int i1, Int i2 -> Bool(i1 > i2)
-         | Float f1, Float f2 -> Bool(f1 > f2)
-         | Int i1,  Float f2 -> Bool(float_of_int i1 > f2)
-         | Float f1, Int i2 -> Bool(f1 > float_of_int i2)
-         | _ -> failwith "Invalid types for comparison")
-     | Lt -> (match v1, v2 with
-         | Int i1, Int i2 -> Bool(i1 < i2)
-         | Float f1, Float f2 -> Bool(f1 < f2)
-         | Int i1,  Float f2 -> Bool(float_of_int i1 < f2)
-         | Float f1, Int i2 -> Bool(f1 < float_of_int i2)
-         | _ -> failwith "Invalid types for comparison")
-     | Geq -> (match v1, v2 with
-         | Int i1, Int i2 -> Bool(i1 >= i2)
-         | Float f1, Float f2 -> Bool(f1 >= f2)
-         | Int i1,  Float f2 -> Bool(float_of_int i1 >= f2)
-         | Float f1, Int i2 -> Bool(f1 >= float_of_int i2)
-         | _ -> failwith "Invalid types for comparison")
-     | Leq -> (match v1, v2 with
-         | Int i1, Int i2 -> Bool(i1 <= i2)
-         | Float f1, Float f2 -> Bool(f1 <= f2)
-         | Int i1,  Float f2 -> Bool(float_of_int i1 <= f2)
-         | Float f1, Int i2 -> Bool(f1 <= float_of_int i2)
-         | _ -> failwith "Invalid types for comparison"))
-  | Unop (e, op) ->
-    let v = eval e in
-    (match op with
-     | Incr -> (match v with
-         | Int i -> Int(i + 1)
-         | _ -> failwith "Can only increment ints")
-     | Decr -> (match v with
-         | Int i -> Int(i - 1)
-         | _ -> failwith "Can only decrement ints and floats")
-     | Not -> (match v with
-         | Bool b -> Bool(not b)
-         | Int i -> Bool(i = 0)
-         | Float f -> Bool(f = 0.0)
-         | Text t -> Bool(t = "")
-         | Label l -> Bool(l = "")
-         | _ -> failwith "Invalid type for negation"))
-     | AddItem (v, item_expr) ->
-        add_item_to_queue action_queue v item_expr
+
+let rec check_expr = function
+    IntLit l -> (TInt, SIntLit l)
+    | TextLit l -> (TString, STextLit l)
+    | BoolLit l -> (TBool, SBoolLit l)
+    | LabelLit l -> (TLabel, SLabelLit l)
+    | FloatLit l -> (TFloat, SFloatLit l)
+    | Node l -> (TNode, SNode l)
+    | Asn(var, e) as ex ->
+        let lt = ident_type var
+        and (rt, e') = check_expr e in
+        let err = "Cannot assign " ^ string_of_typ lt ^ " = " ^
+                  string_of_typ rt ^ " in " ^ string_of_expr ex
+        in
+        (check_assign lt rt err, SAsn(var, (rt, e')))
+
+    | Binop(e1, op, e2) as e ->
+            let (t1, e1') = check_expr e1
+            and (t2, e2') = check_expr e2 in
+            let err = "illegal binary operator " ^
+                    string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                    string_of_typ t2 ^ " in " ^ string_of_expr e
+            in
+            (* All binary operators require operands of the same type*)
+            if t1 = t2 then
+            (* Determine expression type based on operator and operand types *)
+            let t = match op with
+                Add | Sub | Mul | Div when t1 = Int -> Int
+                | Eq -> Bool
+                | Lt | Gt | Leq | Geq when t1 = Int -> Bool
+                | _ -> raise (Failure err)
+            in
+            (t, SBinop((t1, e1'), op, (t2, e2')))
+            else raise (Failure err)
+
+
+(* more to come *)
