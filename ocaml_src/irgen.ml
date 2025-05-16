20e00ba48f6550516a484c728901076d57872333
Dallas Scott <ds4015@columbia.edu>
2025-05-16 05:28:09 -0400

    irgen - store nodes

--- ocaml_src/irgen.ml
+++ ocaml_src/irgen.ml
@@ -35,6 +35,80 @@ let translate (globals, sprogram) =
   let _ = L.struct_set_body narr_t
     [| str_t; str_t; str_t |] false in
 
+
+  let max_entries = 256 in
+
+  (* narrative table *)
+  let narr_arr_ty  = L.array_type    (L.pointer_type narr_t) max_entries in
+  let narr_array   = L.define_global "narr_arr" (L.const_null narr_arr_ty) the_mod in
+  let narr_idx     = L.define_global "narr_idx" (L.const_int i32_t 0) the_mod in
+
+  let narr_add_ft  = L.function_type (L.void_type ctx) [| L.pointer_type narr_t |] in
+  let narr_add_fn  = L.define_function "NarrativeTable_add" narr_add_ft the_mod in
+  let bldr_narr    = L.builder_at_end ctx (L.entry_block narr_add_fn) in
+
+  (* Now use the bound `narr_idx` directly: *)
+  let curr_n = L.build_load narr_idx "curr" bldr_narr in
+  let slot_n = L.build_in_bounds_gep narr_array
+                [| L.const_int i32_t 0; curr_n |]
+                "slot" bldr_narr in
+  let ptr_n  = L.param narr_add_fn 0 in
+  ignore (L.build_store ptr_n slot_n bldr_narr);
+  let next_n = L.build_add curr_n (L.const_int i32_t 1) "next" bldr_narr in
+  ignore (L.build_store next_n narr_idx bldr_narr);
+  ignore (L.build_ret_void bldr_narr);
+
+  (* item table *)
+  let item_arr_ty = L.array_type    (L.pointer_type item_t) max_entries in
+  let item_array  = L.define_global "item_arr" (L.const_null item_arr_ty) the_mod in
+  let item_idx    = L.define_global "item_idx" (L.const_int i32_t 0) the_mod in
+
+  let item_add_ft = L.function_type (L.void_type ctx) [| L.pointer_type item_t |] in
+  let item_add_fn = L.define_function "ItemTable_add" item_add_ft the_mod in
+  let bldr_item   = L.builder_at_end ctx (L.entry_block item_add_fn) in
+  let curr_i = L.build_load item_idx "curr_idx" bldr_item in
+  let slot_i = L.build_in_bounds_gep item_array [| L.const_int i32_t 0; curr_i |] "slot" bldr_item in
+  let ptr_i  = L.param item_add_fn 0 in
+  ignore (L.build_store ptr_i slot_i bldr_item);
+  let next_i = L.build_add curr_i (L.const_int i32_t 1) "next_idx" bldr_item in
+  ignore (L.build_store next_i item_idx bldr_item);
+  ignore (L.build_ret_void bldr_item);
+
+  (* character table *)
+  let chr_arr_ty = L.array_type    (L.pointer_type chrctr_t) max_entries in
+  let chr_array  = L.define_global "chr_arr" (L.const_null chr_arr_ty) the_mod in
+  let chr_idx    = L.define_global "chr_idx" (L.const_int i32_t 0) the_mod in
+
+  let chr_add_ft = L.function_type (L.void_type ctx) [| L.pointer_type chrctr_t |] in
+  let chr_add_fn = L.define_function "CharacterTable_add" chr_add_ft the_mod in
+  let bldr_chr   = L.builder_at_end ctx (L.entry_block chr_add_fn) in
+  let curr_c = L.build_load chr_idx "curr_idx" bldr_chr in
+  let slot_c = L.build_in_bounds_gep chr_array [| L.const_int i32_t 0; curr_c |] "slot" bldr_chr in
+  let ptr_c  = L.param chr_add_fn 0 in
+  ignore (L.build_store ptr_c slot_c bldr_chr);
+  let next_c = L.build_add curr_c (L.const_int i32_t 1) "next_idx" bldr_chr in
+  ignore (L.build_store next_c chr_idx bldr_chr);
+  ignore (L.build_ret_void bldr_chr);
+
+  (* node table *)
+  let node_arr_ty = L.array_type    (L.pointer_type node_t) max_entries in
+  let node_array  = L.define_global "node_arr" (L.const_null node_arr_ty) the_mod in
+  let node_idx    = L.define_global "node_idx" (L.const_int i32_t 0) the_mod in
+  let node_add_ft       = L.function_type (L.void_type ctx) [| L.pointer_type node_t |] in
+  let node_table_add_fn = L.define_function "NodeTable_add" node_add_ft the_mod in
+  let bldr_node         = L.builder_at_end ctx (L.entry_block node_table_add_fn) in
+  let curr_nod = L.build_load node_idx  "curr_idx" bldr_node in
+  let slot_nod = L.build_in_bounds_gep
+                    node_array
+                    [| L.const_int i32_t 0; curr_nod |]
+                    "slot" bldr_node in
+  let ptr_nod  = L.param node_table_add_fn 0 in
+  ignore (L.build_store ptr_nod slot_nod bldr_node);
+  let next_nod = L.build_add curr_nod (L.const_int i32_t 1) "next_idx" bldr_node in
+  ignore (L.build_store next_nod node_idx bldr_node);
+  ignore (L.build_ret_void bldr_node);
+
+
   let rec ltype_of_typ = function
     | TInt        -> i32_t
     | TBool       -> i1_t
@@ -77,7 +151,7 @@ let translate (globals, sprogram) =
           | A.Lt  -> L.build_icmp L.Icmp.Slt
           | _    -> failwith "unhandled binary op"
         in instr a' b' "tmp" builder
-    | SUnop(a,op)    ->
+    | SUnop(a,op) ->
         let a' = build_expr builder a in
         begin match op with
         | A.Incr -> L.build_add a' (L.const_int i32_t 1) "tmp" builder
@@ -85,11 +159,11 @@ let translate (globals, sprogram) =
         | A.Not  -> L.build_not a' "tmp" builder
         | _      -> failwith "unhandled unary op"
         end
-    | SVar sym       ->
+    | SVar sym ->
         let ptr = try StringMap.find sym.name global_vars
                   with Not_found -> failwith ("unknown global "^sym.name) in
         L.build_load ptr sym.name builder
-    | SAsn(sym,e)    ->
+    | SAsn(sym,e) ->
         let v = build_expr builder e in
         let ptr = try StringMap.find sym.name global_vars
                   with Not_found -> failwith ("unknown global "^sym.name) in
@@ -99,6 +173,49 @@ let translate (globals, sprogram) =
     | SNodeBlock(_,exprs,_) 
     | SNodeStream(_,exprs,_)->
         List.fold_left (fun _ e -> build_expr builder e) (L.const_int i32_t 0) exprs
+| SNde nd ->
+        let ch   = L.build_global_stringptr nd.character "who" builder in
+        let nid  = L.const_int             i32_t nd.id in
+        let dlg  = L.build_global_stringptr nd.dialogue "dlg" builder in
+        let lbl  = L.build_global_stringptr nd.label    "lbl" builder in
+        let opts = L.const_null                         tuple_ptr in
+        let nxt  = L.const_int             i32_t nd.next in
+
+        let node_ptr = L.build_malloc node_t "node" builder in
+
+        let f0 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 0 |]
+                   "f0" builder in
+        ignore (L.build_store ch  f0 builder);
+
+        let f1 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 1 |]
+                   "f1" builder in
+        ignore (L.build_store nid f1 builder);
+
+        let f2 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 2 |]
+                   "f2" builder in
+        ignore (L.build_store dlg f2 builder);
+
+        let f3 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 3 |]
+                   "f3" builder in
+        ignore (L.build_store lbl f3 builder);
+
+        let f4 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 4 |]
+                   "f4" builder in
+        ignore (L.build_store opts f4 builder);
+
+        let f5 = L.build_in_bounds_gep node_ptr
+                   [| L.const_int i32_t 0; L.const_int i32_t 5 |]
+                   "f5" builder in
+        ignore (L.build_store nxt f5 builder);
+
+        ignore (L.build_call node_table_add_fn [| node_ptr |] "" builder);
+
+        L.const_int i32_t 0
     | _              -> L.const_int i32_t 0
   in
 
