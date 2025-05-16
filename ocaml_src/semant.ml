2f8f67bff7af374ebb7177f473b31836e1c08139
Dallas Scott <ds4015@columbia.edu>
2025-05-16 01:03:32 -0400

    semant.ml

--- ocaml_src/semant.ml
+++ ocaml_src/semant.ml
@@ -1,5 +1,5 @@
 
-(* mnemo smenatic checking *)
+(* menemo smenatic checking *)
 
 open Ast
 open Sast
@@ -14,49 +14,121 @@ let check_assignment leftVType rightVType =
 		raise (Failure ("Type error: left and right types do not match"))
 
 
-let symbols = List.fold_left (fun m (ty, name) -> StringMap.add name ty m)
-    StringMap.add()
-    (* append symbols here *)
+let bindings = [
+  (TNode, "node");
+  (TChar, "character");
+  (TItem, "item");
+  (TNar, "narrative");
+]
+
+let symbols =
+  List.fold_left
+    (fun acc (ty, name) -> StringMap.add name ty acc)
+    StringMap.empty
+    bindings
 
 
-let ident_type s =
-      try StringMap.find s symbols
-      with Not_found -> raise (Failure ("Unknown identifier " ^ s))
-    in
+module SymbolTable = struct
+  let tbl = Hashtbl.create 32
+  let fresh name typ =
+    let s = Symbol.fresh ~name ~typ in
+    Hashtbl.replace tbl name s;
+    s
+  let find name =
+    try Some (Hashtbl.find tbl name) with Not_found -> None
+end
+
 
 let rec check_expr = function
     IntLit l -> (TInt, SIntLit l)
     | TextLit l -> (TString, STextLit l)
     | BoolLit l -> (TBool, SBoolLit l)
-    | LabelLit l -> (TLabel, SLabelLit l)
+    | LabelLit l -> (TString, SLabelLit l)
     | FloatLit l -> (TFloat, SFloatLit l)
-    | Node l -> (TNode, SNode l)
-    | Asn(var, e) as ex ->
-        let lt = ident_type var
-        and (rt, e') = check_expr e in
-        let err = "Cannot assign " ^ string_of_typ lt ^ " = " ^
-                  string_of_typ rt ^ " in " ^ string_of_expr ex
+    | Binop (e1, op, e2) as e ->
+      let (t1, v1) = check_expr e1 and (t2, v2) = check_expr e2 in
+      let err = (
+        "illegal binary operator " ^
+        string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+        string_of_typ t2 ^ " in " ^ string_of_expr e
+      ) in
+      let t = match (op, t1, t2) with
+      | (Add | Sub | Mul | Div), TInt, TInt -> TInt
+      | (Add | Sub | Mul | Div), TFloat, TFloat -> TFloat
+      | Eq, t1', t2' when t1' = t2' -> TBool
+      | (Lt | Leq | Gt | Geq), TInt, TInt -> TBool
+      | (Lt | Leq | Gt | Geq), TFloat, TFloat -> TBool
+      | _ -> raise (Failure err)
+    in
+    (t, SBinop((t1, v1), op, (t2, v2)))
+    | Unop (e1, op) as e ->
+      let (t1, v1) = check_expr e1 in
+      let err = (
+        "illegal unop operator " ^
+        string_of_typ t1 ^ " " ^ string_of_unary_op op ^ " in " ^ string_of_expr e
+      ) in
+      let t = match (op, t1) with
+        | (Incr | Decr), TInt -> TInt
+        | (Incr | Decr), TFloat -> TFloat
+        | Not, TBool -> TBool
+        | Not, TString -> TString
+        | Not, TInt -> TInt
+        | Not, TFloat -> TFloat
+        | _ -> raise (Failure err)
+      in
+      (t, SUnop((t1, v1), op))
+    | Seq exprs ->
+      let checked = List.map check_expr exprs in
+      (TList TUnit, SSeq checked)
+
+    | Tup (e1,e2) ->
+        let (t1,sx1) = check_expr e1
+        and (t2,sx2) = check_expr e2 in
+        (TTup(t1,t2), STup((t1,sx1),(t2,sx2)))
+
+    | Asn (v,e) ->
+        let (t2,sx2) = check_expr e in
+        let t1 = 
+          match SymbolTable.find v with
+          | Some sym -> check_assignment sym.typ t2
+          | None ->
+              t2
         in
-        (check_assign lt rt err, SAsn(var, (rt, e')))
+        let sym = SymbolTable.fresh v t2 in
+        (t2, SAsn(sym,(t2,sx2)))
 
-    | Binop(e1, op, e2) as e ->
-            let (t1, e1') = check_expr e1
-            and (t2, e2') = check_expr e2 in
-            let err = "illegal binary operator " ^
-                    string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
-                    string_of_typ t2 ^ " in " ^ string_of_expr e
-            in
-            (* All binary operators require operands of the same type*)
-            if t1 = t2 then
-            (* Determine expression type based on operator and operand types *)
-            let t = match op with
-                Add | Sub | Mul | Div when t1 = Int -> Int
-                | Eq -> Bool
-                | Lt | Gt | Leq | Geq when t1 = Int -> Bool
-                | _ -> raise (Failure err)
-            in
-            (t, SBinop((t1, e1'), op, (t2, e2')))
-            else raise (Failure err)
+    | Var v ->
+        begin match SymbolTable.find v with
+        | Some sym -> (sym.typ, SVar sym)
+        | None ->
+            let t = StringMap.find v symbols in
+            let sym = SymbolTable.fresh v t in
+            (t, SVar sym)
+        end
 
 
-(* more to come *)
+    | CodeBlock(start_kw, exprs, end_kw) ->
+        let ch = List.map check_expr exprs in
+        (TUnit, SCodeBlock(start_kw, ch, end_kw))
+
+    | NodeBlock(start_kw, exprs, end_kw) ->
+        let ch = List.map check_expr exprs in
+        (TUnit, SNodeBlock(start_kw, ch, end_kw))
+
+    | NodeStream(start_kw, exprs, end_kw) ->
+        let ch = List.map check_expr exprs in
+        (TUnit, SNodeStream(start_kw, ch, end_kw))
+    | Nar n ->
+        (TNar, SNar n)
+
+    | Itm i ->
+        (TItem, SItm i)
+
+    | Chrc c ->
+        (TChar, SChrc c)
+
+    | Nde nd ->
+        (TNode, SNde nd)
+
+    | AddItem (v1,v2) ->
+        (TUnit, SAddItem(v1,v2))
